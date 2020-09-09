72de3f2c42df82b0df59ab287d5342b1f89c34d8
BK <brian_j_king@me.com>
2020-09-08 21:02:49 -0500

    First Commit

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+            //Console.WriteLine("Hello World!");
+        }
+
+
+
+        public static void DisplayMenu()
+        {
+
+            Console.WriteLine("Text Adventure");
+            Console.WriteLine("1  Create Player");
+            Console.WriteLine("2  Start Playing");
+            Console.WriteLine("3  Comabt Test");
+            Console.WriteLine("4  Exit");
+        }
+
+
+        public static void MenuHeader(string header)
+        {
+            Console.Clear();
+            Console.WriteLine($"---- {header} ---");
+        }
+
+        public static void ContinueApp()
+        {
+
+            bool userResponse = taAppUI.GetUserConsent("Would you like to continue?");
+
+            if (userResponse == true)
+            {
+                Console.Clear();
+                DisplayMenu();
+                RunMenuOptions();
+            }
+            else
+                Console.WriteLine("Exit the application");
+            return;
+        }
+
+
+        public static void RunMenuOptions()
+        {
+            string menuItem = taAppUI.ValidateChoices("Select a type in a number from 1 - 14 \n" +
+                                                      "to choose your menu item", new string[] { "1", "2", "3", "4", "5", "6",
+                                                      "7", "8", "9", "10", "11", "12","13","14","15","16"});
+
+
+            switch (menuItem)
+            {
+                case "1":
+                    MenuHeader("Create a Palyer");
+                    Console.WriteLine("Create a Player"); ;
+                    ContinueApp();
+                    break;
+                case "2":
+                    MenuHeader("Start a Game");
+                    Console.WriteLine("Start a Game");
+                    ContinueApp();
+                    break;
+                case "3":
+                    MenuHeader("Combat Test");
+                    ContinueApp();
+                    break;
+                case "16":
+                    MenuHeader("Exit Application");
+                    return;
+
+            }
+
+        }
+
+    }
+
+
+
+    
+}
---
+++ TextAdventure/taAppUI.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventure
+{
+   public class taAppUI
+    {
+
+        public static bool GetUserConsent(string question)
+
+        {
+            string[] yesOptions = { "Y", "YES", "YEP", "SURE", "OK", "YEAH" };
+            string[] noOptions = { "N", "NO", "NOPE", "NAH" };
+
+            while (true)
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine().Trim();
+
+                if (yesOptions.Contains(userResponse.ToUpper()))
+                {
+                    return true;
+                }
+                else if (noOptions.Contains(userResponse.ToUpper()))
+                {
+                    return false;
+                }
+            }
+
+        }
+
+
+        public static string ValidationMenuChoice(string question)
+
+        {
+
+            string[] menuOptions = { "1", "2", "3", "4", "5", "6",
+                                 "7", "8", "9", "10", "11", "12",
+                                 "13","14"};
+
+            while (true)
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine().Trim();
+
+                if (menuOptions.Contains(userResponse))
+                {
+                    return userResponse;
+                }
+
+            }
+        }
+
+        public static int ValidateInteger(string question)
+        {
+            int number = 0;
+            string str = "";
+            while (!int.TryParse(str, out number))
+            {
+                Console.WriteLine(question);
+                str = Console.ReadLine();
+            }
+            return number;
+        }
+
+        public static decimal ValidateDecimal(string question)
+        {
+            string str = "";
+            decimal dec = 0;
+            while (!Decimal.TryParse(str, out dec))
+            {
+                Console.WriteLine(question);
+                str = Console.ReadLine();
+            }
+            return dec;
+        }
+
+        public static string ValidateString(string question)
+        {
+            string str = "";
+            while (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine(question);
+                str = Console.ReadLine();
+            }
+
+            return str;
+        }
+
+        public static string ValidateChoices(string question, string[] validCondition)
+        {
+
+            while (true)
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine().Trim().ToUpper();
+
+                if (validCondition.Contains(userResponse))
+                {
+                    return userResponse;
+                }
+            }
+        }
+
+
+
+    }
+}
